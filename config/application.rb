c73bc71fa56616af33742422b3f69684b83047b6
Tina Kashef <tina_kashef@yahoo.com>
2012-12-18 17:18:08 -0500

    Changed to not precompile assets in config application

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Myapp
@@ -75,4 +75,4 @@ module Myapp
   end
 end
 # Heroku requires this to be false (per railsapps.github.com/rails-heroku-tutorial.html)
-config.assets.initialize_on_precompile=false
+# config.assets.initialize_on_precompile=false
